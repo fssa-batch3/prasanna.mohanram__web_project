3df837593e92c08ea9c2809548aa18b22761a5ca
BrokenPRasanna <116252140+BrokenPRasanna@users.noreply.github.com>
2023-02-07 18:32:42 +0530

    push

---
+++ javascript.js
@@ -0,0 +1,5 @@
+for (let count = 1; count <= 100; count++) {
+
+    console.log(count++  + " Prasanna");
+    console.log(count + " Balaji");
+}
---
+++ variables.js
@@ -0,0 +1,17 @@
+// syntax
+
+var name = "prasanna";
+var age = "18";
+var phone_number = "8667868155";
+
+// let
+
+let name = "prasanna";
+let age = "18";
+let phone_number = "8667868155";
+
+// const
+
+const name ="prasanna";
+const age ="18";
+const phone_number ="8667868155";
